4c5ff24b12052141835d9aa2391525c89bb414c4
Ved Rathavi <rathavived333@gmail.com>
2025-06-06 13:54:44 +0530

    changes for deployment 6

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -4,14 +4,18 @@ export const verifyToken = (req, res, next) => {
   console.log(req.cookies);
   const token = req.cookies.jwt;
   if (!token) {
-    return res.status(401).send("Not Authorised");
+    return res
+      .status(401)
+      .json({ message: "Not Authorised", cookies: req.cookies });
   }
   jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
     if (err) {
+      console.log("JWT error:", err.message);
       return res.status(403).send("Token is not valid!");
     }
     req.userId = payload.userId;
+    console.log("Verified token for user:", payload.userId);
+    console.log({ token });
     next();
   });
-  console.log({ token });
 };
